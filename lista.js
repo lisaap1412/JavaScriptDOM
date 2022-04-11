d235a634151ce4def4633d921217845004e99fb9
Ana Elisa AP <99884118+lisaap1412@users.noreply.github.com>
2022-04-11 20:06:10 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,65 @@
+function DeleteItem(){
+    console.log(this.parentElement)
+    this.parentElement.remove()
+}
+
+function CriarBotaoDelete(){
+    const botao_delete =  document.createElement('button')
+    botao_delete.classList.add("btn", "btn-danger")
+    botao_delete.innerHTML = "Delete"
+    botao_delete.type = "button"
+    botao_delete.addEventListener("click", DeleteItem);
+
+    return botao_delete
+}
+
+function Submit(){
+    const lista = document.querySelector('[data-task]')
+    const valor = document.querySelector('[data-form-input]')
+
+    const novo_item_lista = document.createElement("li")
+    novo_item_lista.className = "list-group-item";
+
+    novo_item_lista.innerHTML = valor.value
+ 
+    novo_item_lista.appendChild(CriarBotaoDelete())
+    lista.appendChild (novo_item_lista)
+
+    document.getElementById("item").value = ""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction(){
+//     console.log("teste inicial");
+// }
+
+// function addItem(){
+//     const inputItem = document.querySelector('[data-form-input]')
+//     const valorItem = inputItem.value
+
+//     const listaDeItems = document.querySelector('[data-task]')
+
+//     novaLabel = document.createElement('label')
+//     novaLabel.innerText = valorItem
+    
+//     novoItem = document.createElement('li')
+
+//     // novoItem.appendChild(criarBotaoConcluir())
+//     novoItem.appendChild(novaLabel)
+//     // novoItem.appendChild(criarBotaoDelete())
+
+//     listaDeItems.appendChild(novoItem)
+
+//     inputItem.value = ""
+//}
