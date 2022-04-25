9c4e28827a9c9013e24a247001d6e27697b07506
Ana Elisa AP <99884118+lisaap1412@users.noreply.github.com>
2022-04-25 19:00:37 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,8 +1,3 @@
-function DeleteItem(){
-    console.log(this.parentElement)
-    this.parentElement.remove()
-}
-
 function CriarBotaoDelete(){
     const botao_delete =  document.createElement('button')
     botao_delete.classList.add("btn", "btn-danger")
@@ -12,6 +7,48 @@ function CriarBotaoDelete(){
 
     return botao_delete
 }
+function DeleteItem(){
+    console.log(this.parentElement)
+    this.parentElement.remove()
+}
+
+
+function CriarBotaoEdit(){
+    const botao_edit =  document.createElement('button')
+    botao_edit.classList.add("btn", "btn-success")
+    botao_edit.innerHTML = "Edit"
+    botao_edit.type = "button"
+    botao_edit.addEventListener("click", EditItem);
+
+    return botao_edit
+} 
+function EditItem(){
+    const novo_item_lista = document.createElement('input');
+    const FatherElement = this.parentElement
+    FatherElement.innerHTML = '';
+    FatherElement.appendChild(novo_item_lista)
+    FatherElement.appendChild(CriarBotaoEdit())
+    FatherElement.appendChild(CriarBotaoSalvar())
+}
+
+
+function CriarBotaoSalvar(){
+    const botao_salvar =  document.createElement('button')
+    botao_salvar.classList.add("btn", "btn-secondary")
+    botao_salvar.innerHTML = "Salvar"
+    botao_salvar.type = "button"
+    botao_salvar.addEventListener("click", SalvarItem);
+
+    return botao_salvar
+} 
+function SalvarItem(){
+    const input = this.parentElement.children[0] 
+    lista = this.parentElement
+    this.parentElement.innerHTML = input.value
+    lista.appendChild(CriarBotaoDelete())
+    lista.appendChild(CriarBotaoEdit())   
+}
+
 
 function Submit(){
     const lista = document.querySelector('[data-task]')
@@ -23,43 +60,8 @@ function Submit(){
     novo_item_lista.innerHTML = valor.value
  
     novo_item_lista.appendChild(CriarBotaoDelete())
+    novo_item_lista.appendChild(CriarBotaoEdit())
     lista.appendChild (novo_item_lista)
 
     document.getElementById("item").value = ""
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function myFunction(){
-//     console.log("teste inicial");
-// }
-
-// function addItem(){
-//     const inputItem = document.querySelector('[data-form-input]')
-//     const valorItem = inputItem.value
-
-//     const listaDeItems = document.querySelector('[data-task]')
-
-//     novaLabel = document.createElement('label')
-//     novaLabel.innerText = valorItem
-    
-//     novoItem = document.createElement('li')
-
-//     // novoItem.appendChild(criarBotaoConcluir())
-//     novoItem.appendChild(novaLabel)
-//     // novoItem.appendChild(criarBotaoDelete())
-
-//     listaDeItems.appendChild(novoItem)
-
-//     inputItem.value = ""
-//}
